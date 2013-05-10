caffa2e4df695af3b46cd6aa7f2d0f5ed1eb5d75
Noah Gilmore <noah.w.gilmore@gmail.com>
2013-05-09 21:05:40 -0700

    initial commit for heroku

---
+++ anonconsearch/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    # url(r'^$', 'anonconsearch.views.home', name='home'),
+    # url(r'^anonconsearch/', include('anonconsearch.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'search.views.home'),
+    url(r'^query/(?P<page_id>\d+)/$', 'search.views.query')
+)
---
+++ search/views.py
@@ -0,0 +1,24 @@
+# Create your views here.
+from django.http import HttpResponse
+from lxml import html
+from urllib2 import urlopen
+from django.utils import simplejson
+from django.shortcuts import render_to_response
+
+
+def home(request):
+    return render_to_response("index.html")
+
+
+def query(request, page_id):
+    print "got here"
+    try:
+        url = urlopen("http://ucb-anoncon.livejournal.com/7788.html?page=" + str(page_id))
+        dom_data = html.parse(url)
+        json = simplejson.loads(dom_data.getroot().cssselect("script#comments_json")[0].text)
+        output = [{"comment": o["article"], "link": o["actions"][1]["href"]} for o in json if "article" in o.keys()]
+        return HttpResponse(simplejson.dumps(output), mimetype="application/javascript")
+    except Exception:
+        print "uh oh"
+        return HttpResponse(simplejson.dumps({}), mimetype="application/javascript")
+    # comments += [o["article"] for o in loads(html.parse(urllib2.urlopen("http://ucb-anoncon.livejournal.com/7788.html?page=" + str(i))).getroot().cssselect("script#comments_json")[0].text) if "article" in o.keys()]
