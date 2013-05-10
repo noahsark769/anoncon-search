805d3fde4f8f42cc6852844028e77700e313b038
Noah Gilmore <noah.w.gilmore@gmail.com>
2013-05-09 21:46:49 -0700

    ready to push to heroku hopefully

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/noah/Documents/programming/django/anonconsearch/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
