95e0167b304b0f390abca9fc0b3d9d21757c80fc
Noah Gilmore <noah.w.gilmore@gmail.com>
2013-05-14 16:23:07 -0700

    case matching, xss protection, various things

--- search/views.py
+++ search/views.py
@@ -4,6 +4,7 @@ from lxml import html
 from urllib2 import urlopen
 from django.utils import simplejson
 from django.shortcuts import render_to_response
+from cgi import escape
 
 
 def home(request):
@@ -11,26 +12,24 @@ def home(request):
 
 
 def query(request, page_id):
-    print "got here"
-    try:
-        url = urlopen("http://ucb-anoncon.livejournal.com/7788.html?page=" + str(page_id))
-        dom_data = html.parse(url)
-        json = simplejson.loads(dom_data.getroot().cssselect("script#comments_json")[0].text)
-        output = [{"comment": o["article"], "link": o["actions"][1]["href"]} for o in json if "article" in o.keys()]
-        return HttpResponse(simplejson.dumps(output), mimetype="application/javascript")
-    except Exception:
-        print "uh oh"
-        return HttpResponse(simplejson.dumps({}), mimetype="application/javascript")
-    # comments += [o["article"] for o in loads(html.parse(urllib2.urlopen("http://ucb-anoncon.livejournal.com/7788.html?page=" + str(i))).getroot().cssselect("script#comments_json")[0].text) if "article" in o.keys()]
+    output = do_the_querying("http://ucb-anoncon.livejournal.com/7788.html?page=", page_id)
+    return HttpResponse(simplejson.dumps(output), mimetype="application/javascript")
+
 
 def query2(request, page_id):
     print "query 2"
+    output = do_the_querying("http://ucb-anoncon.livejournal.com/7969.html?page=", page_id)
+    return HttpResponse(simplejson.dumps(output), mimetype="application/javascript")
+
+
+def do_the_querying(url, page_id):
     try:
-        url = urlopen("http://ucb-anoncon.livejournal.com/7969.html?page=" + str(page_id))
+        url = urlopen(url + str(page_id))
         dom_data = html.parse(url)
         json = simplejson.loads(dom_data.getroot().cssselect("script#comments_json")[0].text)
-        output = [{"comment": o["article"], "link": o["actions"][1]["href"]} for o in json if "article" in o.keys()]
-        return HttpResponse(simplejson.dumps(output), mimetype="application/javascript")
-    except Exception:
-        print "uh oh"
-        return HttpResponse(simplejson.dumps({}), mimetype="application/javascript")
+        output = [{"comment": escape(o["article"]).replace(escape("<br />"), "<br />"), "link": o["actions"][1]["href"]} for o in json if "article" in o.keys() and o["article"] is not None]
+        return output
+    except Exception as e:
+        print "somethings fucked up mate"
+        print e
+        return {}
